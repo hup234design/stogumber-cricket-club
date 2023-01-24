4670ab52c83af961aff5d0b9ec8a9cd91b2da928
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-01-24 14:22:19 +0000

    fix mysql max key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // fix "max key length is 1000 byte" issue due to older MySql version
+        Schema::defaultStringLength(191);
     }
 }
