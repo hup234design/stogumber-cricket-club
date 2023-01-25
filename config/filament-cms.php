937781f0435129ebe8c78272fd34aa9f44295c76
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-01-25 08:53:01 +0000

    WIP

---
+++ config/filament-cms.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'seed' => env('FILAMENT_CMS_SEED', false),
+
+    'media' => [
+        'variants' => [
+            'thumbnail' => [
+                'aspect_ratio' => '1/1',
+                'width' => 400,
+                'height' => 400
+            ],
+            'featured' => [
+                'aspect_ratio' => '16/9',
+                'width' => 1440,
+                'height' => 810,
+            ],
+            'banner' => [
+                'aspect_ratio' => '4/1',
+                'width' => 1920,
+                'height' => 480
+            ],
+        ],
+    ],
+];
